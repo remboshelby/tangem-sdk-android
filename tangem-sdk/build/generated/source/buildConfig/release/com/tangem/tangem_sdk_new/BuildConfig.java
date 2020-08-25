f360fdd4def70ee18954a12bc8ea1c9a2d472425
Mobile Tangem <mobile@tangem.com>
2020-08-25 22:58:25 +0300

    Release 0.11.0

--- tangem-sdk/build/generated/source/buildConfig/release/com/tangem/tangem_sdk_new/BuildConfig.java
+++ tangem-sdk/build/generated/source/buildConfig/release/com/tangem/tangem_sdk_new/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.tangem.tangem_sdk_new";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "0.0.1";
+  public static final String VERSION_NAME = "1.44.0";
 }
