1c8a04c4c8db53a8ff757857aaaadaadf20c154e
Anton Zhilenkov <azhilenkov@tangem.com>
2020-10-21 00:56:34 +0300

    CSK-73 - the FlipAnimator: can animate a view to the specific side - the TapAnimator: can notify about general animation states; restores view attributes if animation was cancelled - the TouchCardAnimation can place the cardView at a nfc position without animation - minor animation fixes

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/extensions/NfcLocation.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/extensions/NfcLocation.kt
@@ -5,7 +5,5 @@ import com.tangem.tangem_sdk_new.ui.NfcLocation
 /**
  * Created by Anton Zhilenkov on 13/10/2020.
  */
-fun NfcLocation.getX(): Float = this.x / 100f
-fun NfcLocation.getY(): Float = this.y / 100f
 fun NfcLocation.isHorizontal(): Boolean = orientation == 0
 fun NfcLocation.isOnTheBack(): Boolean = z == 0
