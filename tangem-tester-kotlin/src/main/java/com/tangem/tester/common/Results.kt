7414a0d900b6346b454f5ed62c9443cbf62f30d5
Anton Zhilenkov <azhilenkov@tangem.com>
2021-05-11 20:21:10 +0300

    CSK-141 simplified steps. Implement asserts launcher.

--- tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Results.kt
+++ tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Results.kt
@@ -4,11 +4,6 @@ package com.tangem.tester.common
  * Created by Anton Zhilenkov on 19/04/2021.
  */
 sealed class TestResult {
-    class Success : TestResult()
+    class Success(val name: String? = null) : TestResult()
     class Failure(val error: TestFrameworkError) : TestResult()
-}
-
-sealed class StepResult : TestResult() {
-    class Success(val name: String) : StepResult()
-    class Failure(val error: TestFrameworkError) : StepResult()
 }
--- tangem-tester-kotlin/src/main/java/com/tangem/tester/executable/Executable.kt
+++ tangem-tester-kotlin/src/main/java/com/tangem/tester/executable/Executable.kt
@@ -1,13 +1,11 @@
 package com.tangem.tester.executable
 
-import com.tangem.CardSession
-import com.tangem.tester.common.StepResult
-import com.tangem.tester.common.VariableHolder
+import com.tangem.tester.common.ExecutableError
 
 /**
  * Created by Anton Zhilenkov on 18/04/2021.
  */
-interface Executable : VariableHolder {
+interface Executable {
     fun getName(): String
-    fun run(session: CardSession, callback: (StepResult) -> Unit)
+    fun fetchVariables(name: String): ExecutableError?
 }
