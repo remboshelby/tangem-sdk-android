9813c6f71bd7168cdc6c5f98bc1c778cf88c7737
Anton Zhilenkov <azhilenkov@tangem.com>
2021-05-28 21:01:33 +0300

    CSK-161 removed cardTester

--- tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Results.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tester.common
-
-/**
- * Created by Anton Zhilenkov on 19/04/2021.
- */
-sealed class TestResult {
-    class Success(val name: String? = null) : TestResult()
-    class Failure(val error: TestFrameworkError) : TestResult()
-}
